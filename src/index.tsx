f6a722333c4e07bfeb3aa07f584371b9676eb51e
Maxi Eric <maxi_.carp@hotmail.com>
2022-11-06 18:17:38 -0300

    add material

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
-import React from "react";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from 'react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Box } from '@mui/material';
 
-const rootElement = document.getElementById("root") as HTMLElement;
+const rootElement = document.getElementById('root') as HTMLElement;
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <h1>React</h1>
-  </StrictMode>
+    <Box>
+      <h1>React</h1>
+    </Box>
+  </StrictMode>,
 );
